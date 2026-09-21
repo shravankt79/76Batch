3bfd985770fc61fd3dab74505884d7c809a95d4f
shravankt79 <shravankt79@gmail.com>
2026-09-20 23:52:42 -0500

    TC001 is modified in vscode

--- tests/TC001.spec.ts
+++ tests/TC001.spec.ts
@@ -8,5 +8,6 @@ test('@Smoke_Login_Logout', async ({ page })=>{
     await obj.openApplication();
     await obj.login();
     await obj.logout();
+    console.log("Login and Logout test completed successfully");
 });
 
